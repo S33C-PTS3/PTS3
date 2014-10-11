fb101bcad7aab4517807a5d907d4431b432f7f49
JoepKerste <joep_ned@live.nl>
2014-10-11 21:02:07 +0200

    Added Agenda Project for opdracht 6-11 Added class structure with packages

---
+++ Agenda/src/agenda/domain/Appointment.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package agenda.domain;
+
+import fontys.time.*;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Joep Kerste
+ */
+public class Appointment {
+    private String subject;
+    private IPeriod period;
+    private ArrayList<Contact> invited;
+    
+    
+}
