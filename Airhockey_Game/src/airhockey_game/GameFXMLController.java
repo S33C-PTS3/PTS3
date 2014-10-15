9a3c101af6561ca3f87dd8c4ff59d2be6f5a8655
JoepKerste <joep_ned@live.nl>
2014-10-15 16:33:03 +0200

    Rebuilt and fixed GameFXMLController javadoc

--- Airhockey_Game/src/airhockey_game/GameFXMLController.java
+++ Airhockey_Game/src/airhockey_game/GameFXMLController.java
@@ -31,6 +31,8 @@ public class GameFXMLController implements Initializable {
     
     /**
      * Initializes the controller class.
+     * @param url
+     * @param rb
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
