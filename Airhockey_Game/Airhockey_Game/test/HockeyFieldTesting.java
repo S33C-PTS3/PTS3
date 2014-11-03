f2e0142a2d2d00daf0e20b5c9549457e6068c1f2
JoepKerste <joep_ned@live.nl>
2014-11-03 16:07:51 +0100

    Updated testConstructor()

--- Airhockey_Game/Airhockey_Game/test/HockeyFieldTesting.java
+++ Airhockey_Game/Airhockey_Game/test/HockeyFieldTesting.java
@@ -18,10 +18,39 @@ import org.junit.*;
  */
 public class HockeyFieldTesting {
     
+    HockeyField field;
+    Puck puck;
+    Side[] sides;
+    
+    @Before
+    public void setUp()
+    {
+        MySketch applet = new MySketch();
+        applet.init();
+        puck = new Puck(null);
+        
+        //Create players
+        IPlayer h = new HumanPlayer("human");
+        IPlayer r1 = new RobotPlayer("roboteen");
+        IPlayer r2 = new RobotPlayer("robottwee");
+        
+        //Create and add sides
+        sides = new Side[3];
+        Side side1 = new Side(0,0,0,0,Color.BLACK,applet,SideName.BOTTOM,h);
+        sides[0] = side1;
+        Side side2 = new Side(0,0,0,0,Color.BLACK,applet,SideName.RIGHT,r1);
+        sides[1] = side2;
+        Side side3 = new Side(0,0,0,0,Color.BLACK,applet,SideName.LEFT,r2);
+        sides[2] = side3;
+        
+        //Create hockey field
+        field = new HockeyField(puck, sides, null);
+    }
+    
     @Test
     public void testConstructor()
     {
-        //TODO
+        assertNotNull("Field was not instantiated", field);
     }
     
     @Test
@@ -33,27 +62,6 @@ public class HockeyFieldTesting {
     @Test
     public void testCheckWinner()
     {
-        MySketch applet = new MySketch();
-        applet.init();
-        Puck puck = new Puck(null);
-        
-        //Create players
-        IPlayer h = new HumanPlayer("human");
-        IPlayer r1 = new RobotPlayer("roboteen");
-        IPlayer r2 = new RobotPlayer("robottwee");
-        
-        //Create and add sides
-        Side[] sides = new Side[3];
-        Side side1 = new Side(2,1,2,1,Color.BLACK,applet,SideName.BOTTOM,h);
-        sides[0] = side1;
-        Side side2 = new Side(0,0,0,0,Color.BLACK,applet,SideName.RIGHT,r1);
-        sides[1] = side2;
-        Side side3 = new Side(0,0,0,0,Color.BLACK,applet,SideName.LEFT,r2);
-        sides[2] = side3;
-        
-        //Create hockey field
-        HockeyField field = new HockeyField(puck, sides, null);
-        
         //Simulate 10 rounds
         for (int i = 0; i < 10; i++) 
         {
