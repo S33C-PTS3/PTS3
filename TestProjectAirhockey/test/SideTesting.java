c564f3c21d78f0f05adc07cc322fa3543d78056e
JoepKerste <joep_ned@live.nl>
2014-12-08 15:45:07 +0100

    Fixed SideTesting

--- TestProjectAirhockey/test/SideTesting.java
+++ TestProjectAirhockey/test/SideTesting.java
@@ -3,30 +3,52 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-import airhockey.domain.*;
-import airhockey_game.MySketch;
-import java.awt.Color;
+import Game.HumanPlayer;
+import Game.IPlayer;
+import Game.Side;
+import Game.SideName;
+import javafx.geometry.Point2D;
+import javafx.scene.paint.Color;
 import org.junit.*;
 /**
  *
  * @author Joep Kerste
  */
 public class SideTesting {
+    Side side;
+    IPlayer newPlayer;
+
+    @Before
+    public void setUp()
+    {
+        newPlayer = new HumanPlayer();
+        side = new Side(100, 200, 200, 100, Color.GREEN, SideName.RIGHT, newPlayer);
+    }
+    
     @Test
     public void testConstructor()
-    {
-        IPlayer newPlayer = new HumanPlayer("Eric");
-        MySketch ms = new MySketch();
-        ms.init();
-        System.out.println(ms);
-        Side side = new Side(100, 200, 200, 100, Color.GREEN, ms, SideName.RIGHT, newPlayer);
-        
-        
-        Assert.assertEquals("Toegewezen speler niet correct", newPlayer, side.getBindedPlayer());
-        Assert.assertEquals(100, side.getLineX1(), 0);
-        Assert.assertEquals(200, side.getLineX2(), 0);
-        Assert.assertEquals(200, side.getLineY1(), 0);
-        Assert.assertEquals(100, side.getLineY2(), 0);
+    {   
+        Assert.assertEquals("Binded player incorrect", newPlayer, side.getBindedPlayer());
+        Assert.assertEquals("LineX1 incorrect", 100, side.getLineX1(), 0);
+        Assert.assertEquals("LineX2 incorrect", 200, side.getLineX2(), 0);
+        Assert.assertEquals("LineY1 incorrect", 200, side.getLineY1(), 0);
+        Assert.assertEquals("LineY2 incorrect", 100, side.getLineY2(), 0);
         Assert.assertEquals("Sidename incorrect ", SideName.RIGHT, side.getSideName());
+        Assert.assertEquals("Color incorrect ", Color.GREEN, side.getColor());
+        Assert.assertNotNull(side.getBat());
     }  
+    
+    @Test
+    public void testGetStartPoint()
+    {
+        Point2D p = new Point2D(100,200);
+        Assert.assertEquals("Startpoint incorrect", p, side.getStartPoint());
+    }
+    
+    @Test
+    public void testGetEndPoint()
+    {
+        Point2D p = new Point2D(200,100);
+        Assert.assertEquals("Endpoint incorrect", p, side.getEndPoint());
+    }
 }
