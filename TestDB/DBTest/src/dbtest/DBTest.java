60ad15fa117865ac26d2400c7fe88148363a395e
JoepKerste <joep_ned@live.nl>
2014-12-09 15:03:39 +0100

    Updated DB Test

--- TestDB/DBTest/src/dbtest/DBTest.java
+++ TestDB/DBTest/src/dbtest/DBTest.java
@@ -7,7 +7,6 @@ package dbtest;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
